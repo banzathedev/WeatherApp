245bfe7b1c44689db6e31fc036896093b137663b
Giovanni Raposo <giovanniraposo@MacBook-Pro-de-Giovanni.local>
2021-08-04 21:36:22 -0300

    Initial UI/UX Design

---
+++ app/src/main/java/com/proway/whereisthesunapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.proway.whereisthesunapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
